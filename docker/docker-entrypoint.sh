1d6701ce4193d0098fbb9bc9fe5064dd83a07bfe
c6supper <c6supper@hotmail.com>
2020-12-25 11:20:33 +0000

    export PATH before build

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,8 +5,13 @@ set -ex
 patch_conf() {
 
   cd /root/ && tar -xf x-tools.tar.xz && rm -f x-tools.tar.xz;
+
+  # cd $JUPITER_DIR && wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/openssl_1.0.2g.orig.tar.gz;
+  # tar -xf openssl_1.0.2g.orig.tar.gz;
+  # cd openssl-1.0.2g/ && CC=armv7-mx5-linux-gnueabi-gcc ./Configure linux-armv4
+
   # export PKG_CONFIG_PATH=/root/x-tools/armv7-mx5-linux-gnueabi/armv7-mx5-linux-gnueabi/sysroot/usr/lib/pkgconfig/
-  # apt install -y rename locales libc6-i386 lib32stdc++6 subversion git cmake astyle libglib2.0-0:i386 gettext python3
+  # apt install -y rename locales libc6-i386 lib32stdc++6 subversion git cmake astyle libglib2.0-0:i386 gettext python3 libdbus-1-dev
   # locale-gen en_US.UTF-8 && echo "dash dash/sh boolean false" | debconf-set-selections 
   # DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
   # mkdir -p /root/x-tools/armv7-mx5-linux-gnueabi/fakebin/
